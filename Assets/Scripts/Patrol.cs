7e885af42d77c4c23a2094f13241198d057730da
Ben S <bsawtelle94@gmail.com>
2018-07-29 16:53:30 -0700

    First actual commit

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+    public Transform[] patrolPoints;
+
+    public float moveSpeed;
+    private int currentPoint;
+    private int pathDirection;
+
+
+	// Use this for initialization
+	void Start () {
+        transform.position = patrolPoints[0].position;
+        currentPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == patrolPoints[currentPoint].position)
+        {
+            if (currentPoint == (patrolPoints.Length-1))
+            {
+                pathDirection = -1;
+            }
+
+            else if(currentPoint == 0)
+            {
+                pathDirection = 1;
+            }
+
+            currentPoint += pathDirection;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position,
+            moveSpeed * Time.deltaTime);
+
+        print(pathDirection);
+        print(currentPoint)
+;	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+    private float maxSpeed = 70f;
+
+    private Vector3 input;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical"));
+        if (rb.velocity.magnitude <= maxSpeed)
+        {
+            rb.AddForce(input * moveSpeed);
+        }
+        print(input);
+    }
+}
